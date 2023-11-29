c869189f29c6058cb75808fecd133b37d710bc1b
LeKhoaVan <lekhoavan325802@gmail.com>
2023-11-29 13:31:13 +0700

    test

--- cineza_server/src/routes/movieRouter.js
+++ cineza_server/src/routes/movieRouter.js
@@ -3,7 +3,7 @@ const express = require("express");
 const { createMovie, getAllMovie, getByCodeMovie,
     updateMovie, updateStatusMovie, getMovieByDateController, getDateByMovieController,
     getAllMovieForUser } = require("../controller/movieController")
-const { handleUploadFile } = require("../middlewares/upload/uploadImage/index");
+const { handUploadFile } = require("../middlewares/upload/uploadImage/index");
 
 const movieRouter = express.Router();
 
@@ -11,8 +11,8 @@ movieRouter.get("/get-all", getAllMovie);
 movieRouter.get("/:movieCode", getByCodeMovie);
 movieRouter.get("/get-movie-by-date/:date", getMovieByDateController)
 movieRouter.get("/get-date/:codeMovie", getDateByMovieController)
-movieRouter.post("/create", handleUploadFile, createMovie);
-movieRouter.put("/update/:movieCode", handleUploadFile, updateMovie);
+movieRouter.post("/create", handUploadFile, createMovie);
+movieRouter.put("/update/:movieCode", handUploadFile, updateMovie);
 movieRouter.put("/change-status/:movieCode", updateStatusMovie);
 movieRouter.get("/get-all-for-user/:dateCheck", getAllMovieForUser);
 
